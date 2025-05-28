28005f765a34c9e09bf5d978d20f6d0f75c7ffc7
Finley <finleytcostello@gmail.com>
2025-05-28 14:55:26 -0500

    Added process function

--- DataProcessor.py
+++ DataProcessor.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
-df = pd.read_csv('spam.csv',encoding='iso-8859-1')
-df = df[['v1', 'v2']]
-df.columns = ['label', 'message']
+def process(filename):
+    df = pd.read_csv(filename,encoding='iso-8859-1')
+    df = df[['v1', 'v2']]
+    df.columns = ['label', 'message']
 
-vectorizer = CountVectorizer(stop_words='english')
-bow_matrix = vectorizer.fit_transform(df['message'])
+    vectorizer = CountVectorizer(stop_words='english')
+    bow_matrix = vectorizer.fit_transform(df['message'])
 
-bow_df = pd.DataFrame(bow_matrix.toarray(), columns=vectorizer.get_feature_names_out())
-bow_df['label'] = df['label'].values
+    bow_df = pd.DataFrame(bow_matrix.toarray(), columns=vectorizer.get_feature_names_out())
+    bow_df['label'] = df['label'].values
 
-print(bow_df)
+    return bow_df
